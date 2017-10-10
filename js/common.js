433baa7a5ce6e5a11adaece7f2292f3810623eff
Eduard.Zeleniuk <eduard.zeleniuk@gmail.com>
2017-10-10 22:05:37 +0300

    fc

---
+++ js/common.js
@@ -0,0 +1,94 @@
+$(function() {
+
+   $('a').on('click', function(e){
+    e.preventDefault();
+  });
+
+	$('.theTeam__slider').slick({
+		arrows: false,
+  	infinite: true,
+  	dots: true
+	});
+	
+	$('.testimonials__slider').slick({
+		arrows: false,
+  	infinite: true,
+  	autoplay: true,
+  	speed: 4000,
+  	dots: true
+	});
+	
+	$('.portfolio__imgs').isotope({
+  // options
+  itemSelector: '.portfolio__img-wrap',
+  layoutMode: 'masonry'
+});
+
+  function remClass(that) {
+    $('.portfolio__link').removeClass('portfolio__link-active');
+    that.addClass('portfolio__link-active');
+  }
+  var $portfolio = $('.portfolio__imgs');  
+  $('#all').on('click', function() {
+    remClass($(this));
+    $portfolio.isotope({ filter: $('.portfolio__img-wrap')}); 
+  });
+  $('#web').on('click', function() {
+    remClass($(this));
+    $portfolio.isotope({ filter: $('.web')}); 
+  });
+  $('#print').on('click', function() {
+    remClass($(this));
+    $portfolio.isotope({ filter: $('.print')}); 
+  });
+  $('#branding').on('click', function() {
+    remClass($(this));
+    $portfolio.isotope({ filter: $('.branding')}); 
+  });
+  $('#html').on('click', function() {
+    remClass($(this));
+    $portfolio.isotope({ filter: $('.html')}); 
+  });
+
+
+
+
+//map
+
+	var directionsDisplay = new google.maps.DirectionsRenderer(),
+  		marick = new google.maps.LatLng(47.102728, 37.552310),
+  		mapOptions = {
+    								zoom:10,
+    								center: marick
+  		},
+  		map = new google.maps.Map($('.contact__map')[0], mapOptions);
+
+      var marker = new google.maps.Marker({
+                                            position: marick,
+                                            map: map,
+                                            icon: {
+                                                    url:'img/marker-map.png',
+                                                    scaledSize: new google.maps.Size(42, 58)
+                                                  }
+                                          });
+  		directionsDisplay.setMap(map);
+
+	google.maps.event.addDomListener(window, "resize", function() {
+		var center = map.getCenter();
+		google.maps.event.trigger(map, "resize");
+		map.setCenter(center);
+	});
+
+//навигация
+    $(".header, .contactus, .contact__wrap").on("click", "a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top,
+            parent = $(this).closest('.header__nav');
+            if(parent.hasClass('header__nav')) {
+              $('.header__link').removeClass('header__link-active');
+              $(this).addClass('header__link-active');
+            }
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
